5423d1fcdc321decfe5de5d1870a0d55f99177ed
l1004ga <l1004ga@gmail.com>
2023-01-05 19:20:32 +0900

    [Feat] 네이버, 구글 로그인 진행중

--- Bottles/Bottles/BottlesApp.swift
+++ Bottles/Bottles/BottlesApp.swift
@@ -4,20 +4,28 @@
 //
 //  Created by mac on 2023/01/04.
 //
-    				
+// 네이버 SDK 다운 : https://github.com/naver/naveridlogin-sdk-ios/tree/4a7e7a842555f936069a86caaa366667e9b117a6 2020년 버전으로 받아야 함(아님 오류남)
+
+import Foundation
 import SwiftUI
 import FirebaseCore
 //카카오 로그인 SDK
 import KakaoSDKCommon
 import KakaoSDKAuth
+//네이버 로그인 SDK
+import NaverThirdPartyLogin
+
+
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+    
 }
 
 @main
@@ -25,13 +33,41 @@ struct BottlesApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     init() {
-            // Kakao SDK 초기화
-            KakaoSDK.initSDK(appKey: "ac25d3be6ae489fcd49a88acc57f8cf4")
-        }
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: "ac25d3be6ae489fcd49a88acc57f8cf4")
+        
+        //        // Naver SDK Initializing
+        //
+        //        // 네이버 앱으로 로그인 허용
+        //        NaverThirdPartyLoginConnection.getSharedInstance()?.isNaverAppOauthEnable = true
+        //        // 브라우저 로그인 허용
+        //        NaverThirdPartyLoginConnection.getSharedInstance()?.isInAppOauthEnable = true
+        //
+        //        // 네이버 로그인 세로모드 고정
+        //        NaverThirdPartyLoginConnection.getSharedInstance().setOnlyPortraitSupportInIphone(true)
+        //
+        //        // NaverThirdPartyConstantsForApp.h에 선언한 상수 등록
+        //        NaverThirdPartyLoginConnection.getSharedInstance().serviceUrlScheme = kServiceAppUrlScheme
+        //        NaverThirdPartyLoginConnection.getSharedInstance().consumerKey = kConsumerKey
+        //        NaverThirdPartyLoginConnection.getSharedInstance().consumerSecret = kConsumerSecret
+        //        NaverThirdPartyLoginConnection.getSharedInstance().appName = kServiceAppName
+    }
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
+            //                .onOpenURL { url in
+            //                if (NaverThirdPartyLoginConnection
+            //                    .getSharedInstance() != nil)
+            ////                        .isNaverThirdPartyLoginAppschemeURL(url)
+            //                {
+            //                    // Token 발급 요청
+            //                    NaverThirdPartyLoginConnection
+            //                        .getSharedInstance()
+            //                        .receiveAccessToken(url)
+            //                }
+            //    }
+            
         }
     }
 }
