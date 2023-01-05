34742b0f777584ddce2b6fc769aa982f57325128
빵발자 <114224237+yngddo@users.noreply.github.com>
2023-01-06 04:01:46 +0900

    [Feat] Modify BottleDetailView()
    데이터 연결 및 전체적인 구성 수정

--- Bottles/Bottles/View/BottleDetail/BottleDetailView.swift
+++ Bottles/Bottles/View/BottleDetail/BottleDetailView.swift
@@ -7,21 +7,8 @@
 
 import SwiftUI
 
-
-struct ItemInfo1: Codable, Hashable, Identifiable {
-    var id: String = UUID().uuidString
-    var itemDescription: String
-    var itemImage: String
-    var itemName: String
-    var itemPrice: Int
-    var itemTags: [String]
-}
-
 struct BottleDetailView: View {
     
-    let itemInfo1: [ItemInfo1] = [
-        ItemInfo1(itemDescription: "사장님 한마디 하세요..", itemImage: "https://media.discordapp.net/attachments/1013996253283106876/1060431379525017680/036.png", itemName: "맥켈란 1947", itemPrice: 8500000, itemTags: ["1"])
-    ]
     @EnvironmentObject var itemInfoStore: ItemInfoStore
     @State var isSelected: Bool = true
     var bottle: ItemInfo
@@ -29,53 +16,57 @@ struct BottleDetailView: View {
     var body: some View {
         ScrollView {
             VStack {
-                    VStack {
-                        // 상품 이미지
-                        ZStack {
-                            AsyncImage(
-                                url: URL(string: String(bottle.itemImage)),
-                                content: { image in
-                                    image
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 250, height: 250)
-                                        .padding(.bottom, 15)
-                                },
-                                placeholder: {
-                                    ProgressView()
-                                }
-                            )
-                            
-                            // 하트 이미지
-                            .overlay(
-                                Image(systemName: isSelected ? "heart.fill" : "heart")
+                VStack {
+                    // 상품 이미지
+                    ZStack {
+                        AsyncImage(
+                            url: URL(string: String(bottle.itemImage)),
+                            content: { image in
+                                image
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 25, height: 25)
-                                    .offset(x: 160, y: -120)
-                                    .foregroundColor(.accentColor)
-                                    .onTapGesture {
-                                        isSelected.toggle()
-                                    }
-                            )
+                                    .frame(width: 250, height: 250)
+                                    .padding(.bottom, 15)
+                            },
+                            placeholder: {
+                                ProgressView()
+                            }
+                        )
+                        
+                        // 하트 이미지
+                        .overlay(
+                            Image(systemName: isSelected ? "heart.fill" : "heart")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                                .offset(x: 160, y: -120)
+                                .foregroundColor(.accentColor)
+                                .onTapGesture {
+                                    isSelected.toggle()
+                                }
+                        )
+                    }
+                    
+                    // 상품 이름, 가격, 정보
+                    VStack(alignment: .leading, spacing: 17) {
+                        HStack{
+                            Text(bottle.itemName)
+                                .font(.system(size: 22))
+                                .frame(alignment: .leading)
+                                .bold()
                         }
                         
-                        // 상품 이름, 가격, 정보
-                        VStack(alignment: .leading, spacing: 20) {
-                            HStack {
-                                Text(bottle.itemName)
-                                    .font(.system(size: 25))
-                                
-                                Spacer()
-                                
-                                Text("₩ \(bottle.itemPrice)")
-                                    .font(.system(size: 20))
-                            }
-                            .bold()
-                            
-                            Text(bottle.itemVarities)
+                        HStack{
+                            Spacer()
+                            Spacer()
+                            Text("₩ \(bottle.itemPrice)")
+                                .font(.system(size: 18))
+                                .frame(alignment: .trailing)
                         }
+                        
                     }
+                    
+                }
                 
                 Divider()
                     .padding(.top, 10)
@@ -90,27 +81,31 @@ struct BottleDetailView: View {
                     HStack {
                         Text("종류")
                         Divider()
-                        Text("버번 위스키")
+                        Text(bottle.itemType)
                     }
-                    HStack {
-                        Text("용량")
-                        Divider()
-                        Text("750ml")
-                    }
+                    
                     HStack {
                         Text("도수")
                         Divider()
-                        Text("50%")
+                        Text(bottle.itemDegree == "" ? "9~11" : bottle.itemDegree)
                     }
+                    
+                    HStack {
+                        Text("용량")
+                        Divider()
+                        Text("\(bottle.itemML)")
+                    }
+                    
                     HStack {
                         Text("국가")
                         Divider()
-                        Text("미국")
+                        Text(bottle.itemNation)
                     }
+                    
                     HStack {
-                        Text("케이스")
+                        Text("연도")
                         Divider()
-                        Text("없음")
+                        Text(String(bottle.itemYear))
                         Spacer()
                     }
                 }
@@ -132,7 +127,7 @@ struct BottleDetailView: View {
                     HStack {
                         Text("Taste")
                             .frame(width: 50)
-                        Text("부드러운 꿀과 바닐라, 깊고 우아한 다크 초콜릿향")
+                        Text("부드러운 꿀과 바닐라, 깊고 우아한 초콜릿향")
                     }
                     HStack {
                         Text("Finish")
@@ -166,8 +161,8 @@ struct BottleDetailView: View {
 }
 
 
-//struct BottleDetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        BottleDetailView(bottle: ItemInfo(id: "", itemImage: "", itemName: "", itemPrice: 0, itemML: "", itemNation: "", itemProducer: "", itemLocal1: "", itemLocal2: "", itemLocal3: "", itemVarities: "", itemUse: "", itemType: "", itemYear: 0, itemDegree: ""))
-//    }
-//}
+struct BottleDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottleDetailView(bottle: ItemInfo(id: "123123", itemImage: "https://media.discordapp.net/attachments/1013996253283106876/1060431379525017680/036.png", itemName: "123123123123123123123123123123123123", itemPrice: 1, itemML: 1, itemNation: "123123", itemProducer: "12312331", itemLocal1: "123123123", itemLocal2: "", itemLocal3: "123123123", itemVarities: "123123123", itemUse: "123123123", itemType: "123123123", itemYear: 1, itemDegree: "123123123")).environmentObject(ItemInfoStore())
+    }
+}
