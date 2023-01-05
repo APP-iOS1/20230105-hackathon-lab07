f8aae43520fb9405cb7d5acbc721010052bf46f3
FrediOS-dev <114239407+FrediOS-dev@users.noreply.github.com>
2023-01-06 08:35:11 +0900

    [Feat] 검색필터 초기화 구현

---
+++ Bottles/Bottles/View/Map/MapViewSearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewSearchBar.swift
+//  Bottles
+//
+//  Created by BOMBSGIE on 2023/01/06.
+//
+
+import SwiftUI
+
+struct MapViewSearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapViewSearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        MapViewSearchBar()
+    }
+}
