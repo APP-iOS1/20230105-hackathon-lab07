24f70d59016ed342083668b0caf7f0820fc68f01
FrediOS-dev <114239407+FrediOS-dev@users.noreply.github.com>
2023-01-06 08:42:55 +0900

    [Add]MapSearchBar

--- Bottles/Bottles/View/Map/MapViewSearchBar.swift
+++ Bottles/Bottles/View/Map/MapViewSearchBar.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct MapViewSearchBar: View {
+    @EnvironmentObject var shopInfoStore: ShopInfoStore
+    
+    @Binding var mapSearchBarText: String
+
+    @Binding var searchResult: [ShopInfo]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.accentColor)
+                .bold()
+            TextField("검색어를 입력해주세요", text: $mapSearchBarText)
+                .multilineTextAlignment(.center)
+            if !mapSearchBarText.isEmpty {
+                Button(action: {
+                    searchResult = shopInfoStore.shopInfos
+                    self.mapSearchBarText = ""
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                }
+            } else {
+                EmptyView()
+            }
+        }
+        .padding(10)
+        .frame(width: 270)
+        
+        .background{
+            Color.white
+        }
+        .cornerRadius(10)
+        .shadow(color: Color("BottleShopDetailBGShadowColor"), radius: 3, x: 0, y: 4)
+        .padding(.horizontal, 20)
     }
 }
 
-struct MapViewSearchBar_Previews: PreviewProvider {
-    static var previews: some View {
-        MapViewSearchBar()
-    }
-}
+//struct MapViewSearchBar_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapViewSearchBar(mapSearchBarText: .constant(""))
+//    }
+//}
