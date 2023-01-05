4dc5e4f77aa1df4f16ed9bbcfd317fdb534d9961
EN <12171647@inha.edu>
2023-01-05 13:16:27 +0900

    [Add] 기본 그룹 추가, 임시 파일 추가

---
+++ Bottles/Bottles/Store/ShopInfoStore.swift
@@ -0,0 +1,8 @@
+//
+//  ShopInfoStore.swift
+//  Bottles
+//
+//  Created by mac on 2023/01/05.
+//
+
+import Foundation
---
+++ Bottles/Bottles/Store/UserInfoStore.swift
@@ -0,0 +1,8 @@
+//
+//  UserInfoStore.swift
+//  Bottles
+//
+//  Created by mac on 2023/01/05.
+//
+
+import Foundation
