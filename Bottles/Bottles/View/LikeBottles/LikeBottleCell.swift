13b022596ec0b6f3ee53be90eacd88b6675bd486
Jero <62836016+SMizzz@users.noreply.github.com>
2023-01-06 11:04:02 +0900

    [Design] LikeBottleCell Design 수정.

--- Bottles/Bottles/View/LikeBottles/LikeBottleCell.swift
+++ Bottles/Bottles/View/LikeBottles/LikeBottleCell.swift
@@ -30,24 +30,31 @@ struct LikeBottleCell: View {
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 130)
+                        .frame(width: 140, height: 150)
                 } placeholder: {
-                    Image("ready_image")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 170, height: 150)
+                    Rectangle()
+                        .frame(width: 140, height: 150)
+                        .foregroundColor(.gray)
                 }
             VStack(alignment: .leading, spacing: 5) {
                 // 술 이름
                 Text(itemInfo?.itemName ?? "")
                     .font(.title3)
+                    .lineLimit(2)
                     .bold()
                 //바틀샵 상호명
                 Text(shopInfoStore.shopInfos.filter{$0.id == shopId}.first?.shopName ?? "")
+                    .lineLimit(1)
                     .font(.subheadline)
                 // 바틀샵 주소
                 Text(shopInfoStore.shopInfos.filter{$0.id == shopId}.first?.shopAddress ?? "")
+                    .lineLimit(2)
                     .font(.caption)
+                // 술 가격
+                Text("₩ \(itemInfo?.itemPrice ?? 170000)")
+                    .lineLimit(1)
+                    .font(.caption)
+                    .bold()
             }
             .multilineTextAlignment(.leading)
             .foregroundColor(.black)
