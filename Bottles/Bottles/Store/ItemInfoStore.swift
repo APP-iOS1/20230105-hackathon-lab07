287e5012325b8f7aa055d8ac190e1b4029270ca6
빵발자 <114224237+yngddo@users.noreply.github.com>
2023-01-06 09:32:53 +0900

    [Fix]: 0추가함..
    네..

--- Bottles/Bottles/Store/ItemInfoStore.swift
+++ Bottles/Bottles/Store/ItemInfoStore.swift
@@ -21,7 +21,7 @@ class ItemInfoStore: ObservableObject{
             database.collection("Test")
                 .document(shopId).collection("shopItems").getDocuments()
             for document in documents.documents{
-                fetchingResult.append(ItemInfo(id: document.documentID, itemImage: document["imageLink"] as! String, itemName: document["name"] as! String, itemPrice: document["price"] as! Int, itemML: document["ml"] as! Int, itemNation: document["nation"] as! String, itemProducer: document["producer"] as! String, itemLocal1: document["local1"] as! String, itemLocal2: document["local2"] as! String, itemLocal3: document["local3"] as! String, itemVarities: document["varieties1"] as! String, itemUse: document["use"] as! String, itemType: document["type"] as! String, itemYear: document["year"] as? Int ?? , itemAbv: document["abv"] as? String ?? "", itemDegree: document["degree"] as? String ?? ""))
+                fetchingResult.append(ItemInfo(id: document.documentID, itemImage: document["imageLink"] as! String, itemName: document["name"] as! String, itemPrice: document["price"] as! Int, itemML: document["ml"] as! Int, itemNation: document["nation"] as! String, itemProducer: document["producer"] as! String, itemLocal1: document["local1"] as! String, itemLocal2: document["local2"] as! String, itemLocal3: document["local3"] as! String, itemVarities: document["varieties1"] as! String, itemUse: document["use"] as! String, itemType: document["type"] as! String, itemYear: document["year"] as? Int ?? 0, itemAbv: document["abv"] as? String ?? "", itemDegree: document["degree"] as? String ?? ""))
             }
             self.itemInfos = fetchingResult
             
